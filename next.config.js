82f3f09536df4425157bf2b84981994c5307c469
ninomae <sotaro@soutarou-no-iMac.local>
2023-02-24 16:59:21 +0900

    appフォルダ形式はnext exportできないっぽい

--- next.config.js
+++ next.config.js
@@ -1,8 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  experimental: {
-    appDir: true,
+const isProd = process.env.NODE_ENV === 'production'
+
+module.exports = {
+  assetPrefix: isProd ? '/ios16.4-pwa/' : '',
+  images: {
+    unoptimized: true,
   },
 }
-
-module.exports = nextConfig
