3e3a39ebf8fbeb53d6f3dbf86cc7951e698b2f24
西田 宗太郎 <nishida@NISHIDA0565.local>
2023-02-27 14:42:43 +0900

    next public key

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,8 @@
+'use client'
 import Image from 'next/image'
 import { Inter } from 'next/font/google'
 import styles from './page.module.css'
+import firebase from './firebase'
 
 const inter = Inter({ subsets: ['latin'] })
 
