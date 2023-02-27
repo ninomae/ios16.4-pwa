96d6040df7a82c475790d5a230c4625cd97cd2a8
西田 宗太郎 <nishida@NISHIDA0565.local>
2023-02-28 08:15:59 +0900

    manifest json

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,10 +2,15 @@
 import Image from 'next/image'
 import { Inter } from 'next/font/google'
 import styles from './page.module.css'
+import { useEffect } from 'react'
+import { requestPermission } from './firebase'
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
+  useEffect(()=>{
+    requestPermission();
+  },[]);
   return (
     <main className={styles.main}>
       <div className={styles.description}>
