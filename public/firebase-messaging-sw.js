8a8819d044643984b26179926314c2f9ef232f80
西田 宗太郎 <nishida@NISHIDA0565.local>
2023-02-27 15:27:17 +0900

    add firebase messaging-sw.js

---
+++ public/firebase-messaging-sw.js
@@ -0,0 +1,13 @@
+import { getMessaging, getToken } from "firebase/messaging";
+
+// Get registration token. Initially this makes a network call, once retrieved
+// subsequent calls to getToken will return from cache.
+const messaging = getMessaging();
+getToken(messaging, { vapidKey: process.env.NEXT_PUBLIC_CLOUD_MESSAGING_KEY_PAIR }).then((currentToken) => {
+  if (currentToken) {
+  } else {
+    console.log('No registration token available. Request permission to generate one.');
+  }
+}).catch((err) => {
+  console.log('An error occurred while retrieving token. ', err);
+});
