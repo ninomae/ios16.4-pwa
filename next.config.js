900070faa6ef24ad3fc189542df21896144695ed
西田 宗太郎 <nishida@NISHIDA0565.local>
2023-02-27 11:16:38 +0900

    app directory

--- next.config.js
+++ next.config.js
@@ -1,9 +1,8 @@
 /** @type {import('next').NextConfig} */
-const isProd = process.env.NODE_ENV === 'production'
-
-module.exports = {
-  assetPrefix: isProd ? '/ios16.4-pwa/' : '',
-  images: {
-    unoptimized: true,
+const nextConfig = {
+  experimental: {
+    appDir: true,
   },
 }
+
+module.exports = nextConfig
