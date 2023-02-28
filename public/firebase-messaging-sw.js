a11a29637abb2a6bfc976d85a8924617334f0bb5
西田 宗太郎 <nishida@NISHIDA0565.local>
2023-02-28 15:11:15 +0900

    firebase pwa

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -1,7 +1,7 @@
-import { initializeApp } from "firebase/app";
-import { getMessaging, onBackgroundMessage } from "firebase/messaging/sw";
+importScripts('https://www.gstatic.com/firebasejs/8.10.1/firebase-app.js');
+importScripts('https://www.gstatic.com/firebasejs/8.10.1/firebase-messaging.js');
 
-const firebaseApp = initializeApp({
+firebase.initializeApp({
     apiKey: "AIzaSyBTEDDECvB02dTj00GWLeDxUOE5k5aIGck",
     authDomain: "ios16-4pwa.firebaseapp.com",
     projectId: "ios16-4pwa",
@@ -11,17 +11,19 @@ const firebaseApp = initializeApp({
     measurementId: "G-780C37E5PD"
 });
 
-const messaging = getMessaging(firebaseApp);
+const messaging = firebase.messaging();
 
-onBackgroundMessage(messaging, (payload) => {
-  console.log('[firebase-messaging-sw.js] Received background message ', payload);
+messaging.onBackgroundMessage((payload) => {
+  console.log(
+    '[firebase-messaging-sw.js] Received background message ',
+    payload
+  );
   // Customize notification here
   const notificationTitle = 'Background Message Title';
   const notificationOptions = {
     body: 'Background Message body.',
-    icon: '/icon-192x192.png'
+    icon: '/firebase-logo.png'
   };
 
-  self.registration.showNotification(notificationTitle,
-    notificationOptions);
+  self.registration.showNotification(notificationTitle, notificationOptions);
 });
