ddb88674ec74aaedf734ee2edd58bd4bf35932a8
西田 宗太郎 <nishida@NISHIDA0565.local>
2023-02-28 14:09:16 +0900

    request permisioon

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,6 +2,7 @@ import Head from 'next/head'
 import Image from 'next/image'
 import { Inter } from 'next/font/google'
 import styles from '@/styles/Home.module.css'
+import { requestPermission } from './../firebase'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -16,6 +17,9 @@ export default function Home() {
       </Head>
       <main className={styles.main}>
         <div className={styles.description}>
+          <button onClick={requestPermission}>
+            notification
+          </button>
           <p>
             Get started by editing&nbsp;
             <code className={styles.code}>src/pages/index.tsx</code>
